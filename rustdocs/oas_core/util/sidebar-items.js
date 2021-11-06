a0e972ff7d0d9a9dbe286046d7e3b5178ac3c92d
Frando <Frando@users.noreply.github.com>
2021-11-06 21:39:02 +0000

    deploy: bdaa324decd18a2e304650b885ee0e687c90146b

---
+++ rustdocs/oas_core/util/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["MAX_RETRIES",""],["RETRY_INTERVAL",""]],"fn":[["debug_print_record",""],["debug_print_records",""],["wait_for_ready","Repeat a HTTP request until it returns a successfull status."]],"struct":[["RetryOpts",""]]});
